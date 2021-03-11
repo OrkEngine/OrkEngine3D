caa817575b235b671eeaaa8ad2983f2f7a5d89ae
jwoff78 <jwelshoffical78@gmail.com>
2021-03-11 13:46:35 -0600

    Added basic window components, updated files.

--- OrkEngine3D/OrkGame/Program.cs
+++ OrkEngine3D/OrkGame/Program.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using OrkEngine3D;
 namespace OrkGame
 {
     class Program
     {
         static void Main(string[] args)
         {
+            using (Game game = new Game(800, 600, "OrkEngine3D - Test Game"))
+            {
+                game.Run(60.0);
+            }
         }
     }
 }
