b5875bc6dd1a2fe34cf54a4b92030d5ec4a1d09b
jwoff78 <jwelshoffical78@gmail.com>
2021-03-11 13:17:17 -0600

    Add default visual studio project files

---
+++ OrkEngine3D/OrkEngine3D/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrkEngine3D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
