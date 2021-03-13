4a4c91c2d54c60186a12b82dbe21b557a2009f6e
jwoff78 <jwelshoffical78@gmail.com>
2021-03-13 09:20:01 -0600

    Updated to .NET 5.0, concluding updating to OpenTK 4.5.0, last messy update (hopefully)

---
+++ OrkEngine3D/OrkEngine/Engine/Transform.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Mathematics;
+
+namespace OrkEngine.Engine
+{
+    public class Transform
+    {
+        private Vector3 m_position = new Vector3(0, 0, 0);
+        private Vector3 m_rotation = new Vector3(0, 0, 0);
+        private Vector3 m_scale = new Vector3(1, 1, 1);
+
+        public Vector3 Position
+        {
+            get
+            {
+                return m_position;
+            }
+            set
+            {
+                m_position = value;
+            }
+        }
+
+        public Vector3 Rotation
+        {
+            get
+            {
+                return m_rotation;
+            }
+            set
+            {
+                m_rotation = value;
+            }
+        }
+
+        public Vector3 Scale
+        {
+            get
+            {
+                return m_scale;
+            }
+            set
+            {
+                m_scale = value;
+            }
+        }
+    }
+}
--- OrkEngine3D/OrkEngine/Game.cs
+++ OrkEngine3D/OrkEngine/Game.cs
@@ -6,28 +6,39 @@ using System.Threading.Tasks;
 using OpenTK;
 using OpenTK.Graphics;
 using OpenTK.Input;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Desktop;
 
-namespace OrkEngine3D
+namespace OrkEngine
 {
     /// <summary>
     /// Default Game class
     /// </summary>
-    public class Game : GameWindow
+    public class Game// : GameWindow
     {
+
+    } /*: GameWindow
+    { 
+        
         public Game(int width, int height, string title) : base(width, height, GraphicsMode.Default, title) { }
 
         protected override void OnUpdateFrame(FrameEventArgs e)
         {
             KeyboardState input = Keyboard.GetState();
-
+            
             if (input.IsKeyDown(Key.Escape))
             {
                 Exit();
             }
-
+            /*
+             * TODO: game interface 
+             * Have an import of a custom game interface, and plug it in here, so that
+             * the game will run the included interface as game, so onupdateframe doesn't have to be
+             * over written again. Example:
+             * <interfaceclass>.OnUpdate(<args>);
+             
             base.OnUpdateFrame(e);
         }
 
-
-    }
+    }*/
 }
