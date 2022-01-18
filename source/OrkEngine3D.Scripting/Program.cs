278fa288d7fabf55e17c60b795ec4a292d3aee84
jwoff78 <57271436+jwoff78@users.noreply.github.com>
2022-01-18 15:26:49 -0600

    Messing around with javascript scripting engine

---
+++ source/OrkEngine3D.Scripting/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Jint;
+namespace OrkEngine3D.Scripting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var engine = new Engine().SetValue("log", new Action<object>(Console.WriteLine));
+
+            engine.Execute(@"
+function hello() {
+    log('Hello World');
+};
+
+hello();");
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
