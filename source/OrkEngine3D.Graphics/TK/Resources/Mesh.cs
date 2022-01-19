7644ac7eb28c07e6152df973d3e31e80d135313a
khhs167 <khhs1671@gmail.com>
2022-01-19 17:40:31 +0100

    Meshes and shaders++

---
+++ source/OrkEngine3D.Graphics/TK/Resources/Mesh.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using OpenTK.Graphics.OpenGL4;
+using System.Linq;
+using OrkEngine3D.Mathematics;
+
+
+namespace OrkEngine3D.Graphics.TK.Resources
+{
+    public class Mesh : GLResource
+    {
+        int VBO;
+        int VAO;
+        public Vector3[] verticies = new Vector3[0];
+        public Vector2[] uv = new Vector2[0];
+        public Color4[] colors = new Color4[0];
+        public int[] triangles = new int[0];
+        public ShaderProgram shader;
+        public Mesh(GLResourceManager manager) : base(manager)
+        {
+            VBO = GL.GenBuffer();
+            VAO = GL.GenVertexArray();
+        }
+
+        public void UpdateGLData(){
+
+            if(shader == null)
+                throw new NullReferenceException("Shader is null, make sure to set shader before updating data!");
+
+            int floatsperv = 3 + 2 + 4; // Vec3 + Vec2 + Col4
+            float[] bakedData = new float[verticies.Length * floatsperv];
+
+            for (var i = 0; i < verticies.Length; i++)
+            {
+                bakedData[i * floatsperv + 0] = verticies[i].X;
+                bakedData[i * floatsperv + 1] = verticies[i].Y;
+                bakedData[i * floatsperv + 2] = verticies[i].Z;
+                bakedData[i * floatsperv + 3] = (uv.Length > 0 ? uv[i].X : 0);
+                bakedData[i * floatsperv + 4] = (uv.Length > 0 ? uv[i].Y : 0);
+                bakedData[i * floatsperv + 5] = (colors.Length > 0 ? colors[i].Red : 0);
+                bakedData[i * floatsperv + 6] = (colors.Length > 0 ? colors[i].Green : 0);
+                bakedData[i * floatsperv + 7] = (colors.Length > 0 ? colors[i].Blue : 0);
+                bakedData[i * floatsperv + 8] = (colors.Length > 0 ? colors[i].Alpha : 0);
+            }
+
+            GL.BindVertexArray(VAO);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VBO);
+
+            GL.BufferData(BufferTarget.ArrayBuffer, bakedData.Length * sizeof(float), bakedData, BufferUsageHint.StaticDraw);
+
+            int vpos = shader.GetAttribLocation("vPos");
+            int vuv = shader.GetAttribLocation("vUv");
+            int vcol = shader.GetAttribLocation("vCol");
+
+            GL.VertexAttribPointer(vpos, 3, VertexAttribPointerType.Float, false, floatsperv * sizeof(float), 0);
+            GL.EnableVertexAttribArray(vpos);
+
+            GL.VertexAttribPointer(vuv, 2, VertexAttribPointerType.Float, false, floatsperv * sizeof(float), 3);
+            GL.EnableVertexAttribArray(vuv);
+
+            GL.VertexAttribPointer(vcol, 4, VertexAttribPointerType.Float, false, floatsperv * sizeof(float), 5);
+            GL.EnableVertexAttribArray(vcol);
+        
+        }
+
+        public void Render(){
+            GL.BindVertexArray(VAO);
+            shader.Use();
+            GL.DrawArrays(PrimitiveType.Triangles, 0, verticies.Length);
+        }
+
+        public override void Unload()
+        {
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.DeleteBuffer(VBO);
+            GL.DeleteVertexArray(VAO);
+        }
+    }
+}
--- source/OrkEngine3D.Graphics/TK/Window.cs
+++ source/OrkEngine3D.Graphics/TK/Window.cs
@@ -3,12 +3,17 @@ using OpenTK.Windowing.Desktop;
 using OpenTK.Mathematics;
 using OpenTK.Graphics.OpenGL4;
 using System;
+using OrkEngine3D.Graphics.TK.Resources;
+using OrkEngine3D.Mathematics;
+using Vector3 = OrkEngine3D.Mathematics.Vector3;
+using Color4 = OrkEngine3D.Mathematics.Color4;
 
 namespace OrkEngine3D.Graphics.TK
 {
     public class GraphicsContext
     {
         public GameWindow window;
+        public GLResourceManager glmanager;
         public void Run(string title){
             var nws = NativeWindowSettings.Default;
             var gws = GameWindowSettings.Default;
@@ -24,17 +29,46 @@ namespace OrkEngine3D.Graphics.TK
             window.Load += OnLoad;
             window.RenderFrame += OnRender;
             window.UpdateFrame += OnUpdate;
+            window.Unload += Unload;
+
+            glmanager = new GLResourceManager();
 
             window.Run();
         }
-
+        Shader fshader;
+        Shader vshader;
+        ShaderProgram program;
         private void OnLoad(){
+            mesh = new Mesh(glmanager);
+            fshader = new Shader(glmanager, fshadersource, ShaderType.FragmentShader);
+            vshader = new Shader(glmanager, vshadersource, ShaderType.VertexShader);
+
+            program = new ShaderProgram(glmanager, vshader, fshader);
+
+            mesh.shader = program;
+            mesh.verticies = new Vector3[] {
+                new Vector3(-0.5f, -0.5f, 0),
+                new Vector3( 0.0f,  0.5f, 0),
+                new Vector3( 0.5f, -0.5f, 0),
+            };
+
+            mesh.colors = new Color4[] {
+                new Color4(1.0f, 1.0f, 0.0f, 0.0f),
+                new Color4(1.0f, 0.0f, 1.0f, 0.0f),
+                new Color4(1.0f, 0.0f, 0.0f, 1.0f)
+            };
+
+            mesh.UpdateGLData();
             
         }
+
+        Mesh mesh;
 
         private void OnRender(FrameEventArgs e){
 
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            mesh.Render();
 
             window.SwapBuffers();
         }
@@ -42,5 +76,36 @@ namespace OrkEngine3D.Graphics.TK
         private void OnUpdate(FrameEventArgs e){
 
         }
+
+        private void Unload(){
+            glmanager.Unload();
+        }
+
+        string vshadersource = @"
+#version 330 core
+layout (location = 0) in vec3 vPos;
+layout (location = 2) in vec4 vCol;
+
+out vec4 vColor;
+
+void main()
+{
+    gl_Position = vec4(vPos, 1.0);
+    vColor = vCol;
+}
+        ";
+
+        string fshadersource = @"
+#version 330 core
+out vec4 FragColor;
+
+in vec4 vColor;
+
+void main()
+{
+    FragColor = vColor;//vec4(1.0f, 0.5f, 0.2f, 1.0f);
+}
+
+        ";
     }
 }
